394415b6ae2fef53acd75e4cdc72e019cf4b3a84
Miguel Peralta <eudysperalta723@gmail.com>
2020-01-29 07:20:51 -0800

    day 1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,15 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+    
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = info
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +70,19 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        while running:
+            IR = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if IR == HLT:
+                running = False
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                self.pc += 2
+                
